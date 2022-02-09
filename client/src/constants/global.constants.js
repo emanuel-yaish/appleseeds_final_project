79db559a97e114c9c9fd7d31f33303122054f531
Emanuel Yaish <emanuelyaish@gmail.com>
2022-02-09 20:10:46 +0200

    client env

--- client/src/constants/global.constants.js
+++ client/src/constants/global.constants.js
@@ -3,4 +3,5 @@ export const CURRENT_URL =
     ? process.env.REACT_APP_LOCALHOST_URL
     : process.env.REACT_APP_HEROKU_HOST_URL;
 
+console.log("CURRENT_URL", CURRENT_URL);
 console.log(process.env.REACT_APP_NODE_END);
